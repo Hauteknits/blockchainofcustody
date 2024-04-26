6dabec2ef3b4c5b0831300c708fe5573ea02ac2d
Hauteknits <holdenclarke@yahoo.com>
2024-04-26 16:06:38 -0700

    Implemented more methods

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
+import {blockToU8, u8ToBlock, Block, hexToU8} from "./util.js";
 
-var blockArray;
-class Block{
-	constructor(prevBlock){
-		this.hash="f19a5201780e4463db341b02889255fe5a310a5c6a3267440ea6811b3bb33616";
-		this.timestamp=1713978422.209468;
-		this.UUID="e84e339e-5c0f-4f4d-84c5-bb79a3c1d2a2";
-		this.evID="c84e339e-5c0f-4f4d-84c5-bb79a3c1d2a2";
-		this.state="INITIAL";
-		this.creator="\0";
-		this.owner="\0";
-		this.blockLength=14;
-		this.data="Initial Block\0"
-	}
-}
 //testwrite
 let block = new Block();
+block.hash="f19a5201780e4463db341b02889255fe5a310a5c6a3267440ea6811b3bb33616";
+block.timestamp=1713978422.209468;
+block.UUID="e84e339e-5c0f-4f4d-84c5-bb79a3c1d2a2";
+block.evID="c84e339e-5c0f-4f4d-84c5-bb79a3c1d2a2";
+block.state="INITIAL";
+block.creator="HoldenC";
+block.owner="HoldenClarke";
+block.blockLength=14;
+block.data="Initial Block\0"
 
 
-
+let arr = blockToU8(block);
+let newBlock = u8ToBlock(arr);
+console.log(block);
+console.log(newBlock);
+console.log(block.hash.localeCompare(newBlock.hash));
+console.log(block.timestamp == newBlock.timestamp);
+console.log(block.UUID.localeCompare(newBlock.UUID));
+console.log(block.evID.localeCompare(newBlock.evID));
+console.log(block.state.localeCompare(newBlock.state));
+console.log(block.creator.localeCompare(newBlock.creator));
+console.log(block.owner.localeCompare(newBlock.owner));
+console.log(block.blockLength == newBlock.blockLength);
+console.log(block.data.localeCompare(newBlock.data));
 
 
 //Deno.writeFileSync("blockchain", blockArray);
--- util.js
+++ util.js
@@ -1,8 +1,16 @@
-import { crypto } from "https://deno.land/std@0.115.0/crypto/mod.ts";
+import { crypto } from "https://deno.land/std@0.223.0/crypto/mod.ts";
+import { Aes } from "https://deno.land/x/crypto/aes.ts";
+import { Ecb, Padding } from "https://deno.land/x/crypto/block-modes.ts";
+const textEnc = new TextEncoder();
+const textDec = new TextDecoder();
+const AES_KEY = textEnc.encode("R0chLi4uLi4uLi4=");
+
+const cipher = new Ecb(Aes, AES_KEY, Padding.NONE);
+const decipher = new Ecb(Aes, AES_KEY, Padding.NONE);
 
 //https://friendlyuser.github.io/posts/tech/2023/An_Introduction_to_the_Deno_Crypto_Module/
 //AES encryption https://medium.com/deno-the-complete-reference/private-key-aes-encryption-decryption-in-deno-10cf33b41eaf
-const textEnc = new TextEncoder();
+
 
 export function readBlockChain(){
 
@@ -20,7 +28,7 @@ export function writeBlockChain(blockChain){
 export class Block{
 	constructor(prevBlock){
 		this.hash="";
-		this.timestamp=;
+		this.timestamp="";
 		this.UUID="";
 		this.evID="";
 		this.state="";
@@ -30,20 +38,20 @@ export class Block{
 		this.data=""
 	}
 }
-function makeHash(block){
-	return //Sha256.update(previous block to data).hex();
+async function makeHash(blockChain){
+	hash = await crypto.subtle.digest("SHA-256", blockToU8(blockChain.at(blockChain.length-1)));
+	return hash;
 }
 
-function blockToU8(block){
+export function blockToU8(block){
 	let blockArray;
 	//hash
 	blockArray = hexToU8(block.hash, 32);
 	//timestamp
 	blockArray = concatArray(blockArray, new Uint8Array((new Float64Array([block.timestamp])).buffer));
-	console.log(new Uint8Array((new Float64Array([block.timestamp])).buffer));
 	//UUID & Evidence
-	blockArray = concatArray(blockArray, hexToU8(block.UUID,32));
-	blockArray = concatArray(blockArray, hexToU8(block.evID,32));
+	blockArray = concatArray(blockArray, cipher.encrypt(textEnc.encode(block.UUID.replace(/-/g, ""))));
+	blockArray = concatArray(blockArray, cipher.encrypt(textEnc.encode(block.evID.replace(/-/g, ""))));
 	//State, creator, owner
 	blockArray = concatArray(blockArray, pad(textEnc.encode(block.state), 12));
 	blockArray = concatArray(blockArray, pad(textEnc.encode(block.creator), 12));
@@ -52,9 +60,46 @@ function blockToU8(block){
 	blockArray = concatArray(blockArray, new Uint8Array((new Uint32Array([block.blockLength])).buffer));
 	//Data
 	blockArray = concatArray(blockArray, textEnc.encode(block.data));
+	return blockArray;
+}
+export function u8ToBlock(u8){
+	let block = new Block(null);
+	
+	//setup arrays for easy of use
+	let hashA = u8.slice(0,32);
+	let timestampA = u8.slice(32,40);
+	let uuidA = u8.slice(40, 72);
+	let evidA = u8.slice(72, 104);
+	let stateA = u8.slice(104,116);
+	let creatorA = u8.slice(116,128);
+	let ownerA = u8.slice(128, 140);
+	let lengthA = u8.slice(140,144);
+	let dataA = u8.slice(144, u8.length-1);
+
+	//hash
+	block.hash = u8ToHex(hashA);
+	//timestamp
+	block.timestamp = (new Float64Array(timestampA.buffer))[0];
+	//IDs
+	block.UUID = makeUUID(textDec.decode(decipher.decrypt(uuidA)));
+	block.evID = makeUUID(textDec.decode(decipher.decrypt(evidA)));
+	//state, creator, owner
+	block.state = textDec.decode(stateA).replace(/\0/g,"");
+	block.creator = textDec.decode(creatorA).replace(/\0/g,"");
+	block.owner = textDec.decode(ownerA).replace(/\0/g,"");
+	//Length
+	block.blockLength = new DataView(lengthA.buffer, 0).getUint32(0, true);
+	//Data
+	/*Cluster fuck of a method. Pretty much what it does is create an array of "\0" the size of the block length, then concats that on to a character array of the data string.
+	then finally trims that array down to the length of the data and joins back into a string :)*/ 
+	block.data = textDec.decode(dataA).split("").concat(new Array(block.blockLength).fill("\0")).slice(0,block.blockLength).join("")
+	return block;
+
+
+
 }
 
-function hexToU8(str, length){
+export function hexToU8(str, length){
 	let newArr=new Uint8Array(length).fill(0);
 	str = str.replace(/-/g, "");
 	for(let i=0; i < str.length; i+=2){
@@ -62,6 +107,13 @@ function hexToU8(str, length){
 		newArr[i/2]=value;
 	}
 	return newArr;
+}
+function u8ToHex(u8){
+	let str = "";
+	u8.forEach((n)=>{
+		str+= (n.toString(16).length == 1) ? '0'+ n.toString(16) : n.toString(16);
+	});
+	return str;
 }
 function concatArray(arr1, arr2){
 	let r = new Uint8Array(arr1.length+arr2.length);
@@ -73,4 +125,7 @@ function pad(arr, length){
 	let r = new Uint8Array(length).fill(0);
 	r.set(arr);
 	return r;
+}
+function makeUUID(str){
+	return `${str.substr(0,8)}-${str.substr(8,4)}-${str.substr(12,4)}-${str.substr(16,4)}-${str.substr(20)}`;
 }
